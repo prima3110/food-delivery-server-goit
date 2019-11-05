77df3815a9127c6562be31a320ebd1fcce9ac148
Roman Pryymak <prima3110@ukr.net>
2019-11-05 15:58:08 +0200

    added validation

--- hw-01/src/routes/signup/signup.js
+++ hw-01/src/routes/signup/signup.js
@@ -6,24 +6,35 @@ path = require('path');
       
       if(request.method === 'POST') {
           let body = '';
-         await request.on('data', function (data) {
+          await request.on('data', function (data) {
               body = body + data;
-          });
+            });
 
-       const userName = JSON.parse(body);
-       const filePath = path.join(__dirname, '../../', 'db/', 'users/', `${userName.username}.json`);
-       fs.writeFile(filePath, body, err => {
+            const userName = JSON.parse(body);
+
+
+          if(!userName.username || !userName.telephone || typeof(parseInt(userName.telephone)) !== "number" 
+          || !userName.password || !userName.email || !userName.email.includes('@')) {
+            response.writeHead(400, {"Content-Type": "text/plain"});
+            response.write("Bad Request");
+            response.end();
+          } else {
+
+            const filePath = path.join(__dirname, '../../', 'db/', 'users/', `${userName.username}.json`);
+            fs.writeFile(filePath, body, err => {
            if (err) {
                throw err;
            }
-       });
 
-       response.writeHead(200, {'Content-Type': 'application/json'});
-       response.write(`{
+           response.writeHead(201, {'Content-Type': 'application/json'});
+           response.write(`{
             'status': 'success',
             'user': ${body}
            }`);
            response.end();
+       });
+
+          }
       }
   };
 
